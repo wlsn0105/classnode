f39bbc4aa60a5315d14bd3b149de8f544fd8e4b3
wlsn0105 <jack13579@naver.com>
2023-12-04 21:31:40 +0900

    feat

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const QueryString = require("qs");
 const app = express();
-
+id = 0;
+firstname = "john";
 app.listen(8080, function(){
     console.log("포트 8080번으로 대기중...")
 });
@@ -10,3 +12,10 @@ app.get('/book', function(req, res){
     res.send('도서 목록 관련 페이지입니다.');
 })
 // 학교 네트워크는 막혀 있음
+app.get('/checkid', function(req, res){
+    query = req.query
+    // query 문을 이용해서 요청
+    console.log(query)
+    // localhot:8080/checkid를 주소창에 작성하면 query가 출력
+    res.send('도서 목록 관련 페이지입니다.');
+})
